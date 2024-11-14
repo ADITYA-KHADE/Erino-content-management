0aea578ffec93bec693d0e4d1a306e2e584d7e1f
adityakhade321 <adityakhadeak47@gmail.com>
2024-11-15 03:01:34 +0530

    remove unused backend controllers, models, routes, and token generation logic

--- backend/routes/projectRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getProjects,addProject,uploadImage} =require("../controllers/project.controller")
-
-router.post("/getprojects",getProjects);
-router.post("/addproject",uploadImage,addProject);
-
-module.exports = router
--- backend/routes/skillsRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getSkills,addSkill,uploadImages} =require("../controllers/skill.controller")
-
-router.post("/getskills",getSkills);
-router.post("/addskill",uploadImages,addSkill);
-
-module.exports = router
